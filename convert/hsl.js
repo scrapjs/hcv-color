5d6c517860c96a10481a6875791dfbbf9da8f59d
acterhd <acterhd@gmail.com>
2016-07-08 15:02:34 +0800

    HSL

--- convert/hsl.js
+++ convert/hsl.js
@@ -8,7 +8,8 @@
     let rgb2hsl = (rgb) => {
         let [r, g, b] = [rgb[0] / 255, rgb[1] / 255, rgb[2] / 255];
         let [min, max] = [Math.min(r, g, b), Math.max(r, g, b)];
-        let [h, c] = [0, max - min], l = (min + max) / 2, s = c / (1 - Math.abs(2 * l - 1));
+        let [h, c] = [0, max - min], l = (min + max) / 2, s = 0;
+        if (l > 0 && l < 1) s = c / (1 - Math.abs(2 * l - 1));
         if (c > 0) h = [(g - b) / c + (g < b ? 6 : 0), (b - r) / c + 2, (r - g) / c + 4][[r, g, b].indexOf(max)];
         return [h * 60, s * 100, l * 100];
     };
