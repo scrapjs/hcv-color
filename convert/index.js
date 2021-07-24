16db414e97d213cc407b20ba2d61b42079a237d1
helix.d <40030119+helixd-2k18@users.noreply.github.com>
2021-07-25 00:50:28 +0800

    Update index.js

--- convert/index.js
+++ convert/index.js
@@ -6,29 +6,29 @@
 
   const shift = [0.0, 2.0, 4.0];
 
-  let rgb2hcg = (rgb)=>{
+  let rgb2hcv = (rgb)=>{
     let m = Math.min.apply(Math, rgb);
     let M = Math.max.apply(Math, rgb);
     let C = M - m;
     let gbr = rgb.map((v, i, arr)=> { return arr[mod(i - 2, 3)]; });
     let brg = rgb.map((v, i, arr)=> { return arr[mod(i - 1, 3)]; });
-    let G = C < 1 ? m / (1 - C) : 0;
+    let V = C < 1 ? m / (1 - C) : 0;
     let H = C > 0 ? Math.max.apply(Math, rgb.map(function(v, i){
       let a = (gbr[i] - brg[i]) / C;
       let b = mod(a + shift[i], 6.0);
       return b * (M == v);
     })) : 0.0;
-    return [H / 6.0, C, G];
+    return [H / 6.0, C, V];
   };
 
-  let hcg2rgb = ([H, C, G])=>{
+  let hcg2rgb = ([H, C, V])=>{
     let h = H * 6.0;
     let rgb = Array.from([h, h, h]).map(function(v, i){
       let a = mod(v - shift[i], 6.0);
       let b = Math.abs(a - 3.0) - 1.0;
       return Math.min(Math.max(b, 0.0), 1.0);
     });
-    let m = G * (1 - C);
+    let m = V * (1 - C);
     return rgb.map(function(ch){
       return ch * C + m;
     });
