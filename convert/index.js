070b79bdd8d70dcb72c58ac5228ae72bc170ff3a
helix.d <40030119+helixd-2k18@users.noreply.github.com>
2021-07-25 00:52:45 +0800

    Update index.js

--- convert/index.js
+++ convert/index.js
@@ -1,19 +1,17 @@
 (()=>{
 
-  let mod = (a, n) => {
-    return ((a % n) + n) % n;
-  };
+  let mod = (a, n) => { return ((a % n) + n) % n; };
 
   const shift = [0.0, 2.0, 4.0];
 
-  let rgb2hcv = (rgb)=>{
+  let rgb2hcv = (rgb) => {
     let m = Math.min.apply(Math, rgb);
     let M = Math.max.apply(Math, rgb);
     let C = M - m;
-    let gbr = rgb.map((v, i, arr)=> { return arr[mod(i - 2, 3)]; });
-    let brg = rgb.map((v, i, arr)=> { return arr[mod(i - 1, 3)]; });
+    let gbr = rgb.map((v, i, arr) => { return arr[mod(i - 2, 3)]; });
+    let brg = rgb.map((v, i, arr) => { return arr[mod(i - 1, 3)]; });
     let V = C < 1 ? m / (1 - C) : 0;
-    let H = C > 0 ? Math.max.apply(Math, rgb.map(function(v, i){
+    let H = C > 0 ? Math.max.apply(Math, rgb.map((v, i) => {
       let a = (gbr[i] - brg[i]) / C;
       let b = mod(a + shift[i], 6.0);
       return b * (M == v);
@@ -21,17 +19,15 @@
     return [H / 6.0, C, V];
   };
 
-  let hcv2rgb = ([H, C, V])=>{
+  let hcv2rgb = ([H, C, V]) => {
     let h = H * 6.0;
-    let rgb = Array.from([h, h, h]).map(function(v, i){
+    let rgb = Array.from([h, h, h]).map((v, i) => {
       let a = mod(v - shift[i], 6.0);
       let b = Math.abs(a - 3.0) - 1.0;
       return Math.min(Math.max(b, 0.0), 1.0);
     });
     let m = V * (1 - C);
-    return rgb.map(function(ch){
-      return ch * C + m;
-    });
+    return rgb.map((ch) => { return ch * C + m; });
   };
 
   let convert = {rgb2hcv, hcv2rgb};
