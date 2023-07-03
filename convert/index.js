19245fb304c6f947cf4351dd1426ccc2e99465f6
unit-a-user <helixd.2s18@gmail.com>
2023-07-03 10:47:22 +0700

    means floating point

--- convert/index.js
+++ convert/index.js
@@ -6,13 +6,13 @@ export const rgb2hcv = (rgb) => {
     const C = Math.min(Math.max(M - m, 0.0), 1.0);
     const gbr = rgb.map((v, i, arr) => arr[mod(i - 2, 3)]);
     const brg = rgb.map((v, i, arr) => arr[mod(i - 1, 3)]);
-    const V = C < 1 ? m / (1 - C) : 0;
-    const H = C > 0 ? Math.max(...rgb.map((v, i) => (mod((gbr[i] - brg[i]) / C + shift[i], 6.0) * (M == v)))) : 0.0;
+    const V = C < 1.0 ? m / (1.0 - C) : 0.0;
+    const H = C > 0.0 ? Math.max(...rgb.map((v, i) => (mod((gbr[i] - brg[i]) / C + shift[i], 6.0) * (M == v)))) : 0.0;
     return [H / 6.0, C, V];
 };
 export const hcv2rgb = ([H, C, V]) => {
     const h = H * 6.0;
     const rgb = [h, h, h].map((v, i) => Math.min(Math.max(Math.abs(mod(v - shift[i], 6.0) - 3.0) - 1.0, 0.0), 1.0));
-    const m = V * (1 - C);
+    const m = V * (1.0 - C);
     return rgb.map((ch) => ch * C + m);
 };
